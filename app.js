30ce5970372326fd59991658776d65109a103be3
Diego97-webdev <shizusenpai97@gmail.com>
2020-06-01 18:14:01 -0300

    terminado responsive en 768px, cambio de tamaños en tipografias

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var mySwiper = new Swiper(".swiper-container", {
   // Optional parameters
   direction: "horizontal",
   loop: false,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  //PARA PROBAR -> effect: "fade",
 
   // If we need pagination
   pagination: {
@@ -17,5 +22,23 @@ var mySwiper = new Swiper(".swiper-container", {
   // And if we need scrollbar
   scrollbar: {
     el: ".swiper-scrollbar",
+  },
+});
+
+var swiper = new Swiper(".swiper-contenedor", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-puntitos",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
