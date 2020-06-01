9ca418aeee470762ca709eb2c17869596b043413
Diego97-webdev <shizusenpai97@gmail.com>
2020-06-01 15:40:43 -0300

    reemplazado el header slider slick por swiper

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-$(".header-principal").slick({
-  infinite: false,
-  dots: false,
-  arrows: false,
+var mySwiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
 });
