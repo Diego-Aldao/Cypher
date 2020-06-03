d7231733b284ba885f3e33421c43d4c68dbe134c
Diego97-webdev <shizusenpai97@gmail.com>
2020-06-03 18:50:29 -0300

    agregado document.ready para mejorar la carga de la pagina

--- app.js
+++ app.js
@@ -1,49 +1,51 @@
-var mySwiper = new Swiper(".swiper-container", {
-  // Optional parameters
-  direction: "horizontal",
-  speed: 3000,
-  parallax: true,
-  loop: false,
-  autoplay: {
-    delay: 3500,
-    disableOnInteraction: false,
-  },
-  //PARA PROBAR -> effect: "fade",
+document.addEventListener("DOMContentLoaded", function () {
+  var mySwiper = new Swiper(".swiper-container", {
+    // Optional parameters
+    direction: "horizontal",
+    speed: 3000,
+    parallax: true,
+    loop: false,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    //PARA PROBAR -> effect: "fade",
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-});
+    // And if we need scrollbar
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
 
-var swiper = new Swiper(".swiper-contenedor", {
-  spaceBetween: 0,
-  speed: 1500,
-  centeredSlides: true,
-  parallax: true,
-  loop: true,
-  autoplay: {
-    delay: 3500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-puntitos",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+  var swiper = new Swiper(".swiper-contenedor", {
+    spaceBetween: 0,
+    speed: 1500,
+    centeredSlides: true,
+    parallax: true,
+    loop: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-puntitos",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 });
